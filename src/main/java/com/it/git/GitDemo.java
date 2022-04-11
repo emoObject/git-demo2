da70ff7d8fdddd0b0883dcb34bc237dd5600987d
root <root@object.com>
2022-04-11 14:48:02 +0800

    version -2.0

--- src/main/java/com/it/git/GitDemo.java
+++ src/main/java/com/it/git/GitDemo.java
@@ -3,5 +3,6 @@ package com.it.git;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("git Second");
     }
 }
